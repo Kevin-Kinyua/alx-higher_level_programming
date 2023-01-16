e3f7ce4d9b0ee3d3c3cc265fe346d66c5981e0b1
Kevin Kinyua <w.kevinkinyua@gmail.com>
2023-01-16 08:49:10 -0800

    Test models

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is the square.py unittest module."""
+from unittest import TestCase, mock
+from io import StringIO
+
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import 
