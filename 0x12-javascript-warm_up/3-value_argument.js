fff099c6471663e3a0b3e611473e55e752aaa249
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-03-15 19:35:44 +0300

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const len = process.argv.slice(2); // Get the command-line arguments, excluding the first two
-
-if (len[0]) {
-  console.log(len[0]);
-} else {
-  console.log("No argument");
-}
+if (!process.argv[2]) {
+    console.log('No argument');
+  } else {
+    console.log(process.argv[2]);
+  }  
