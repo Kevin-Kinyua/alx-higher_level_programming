9be1193d075a7d8e9ced70538340184a18fce4fc
Kevin Wangome Kinyua <w.kevinkinyua@gmail.com>
2023-03-31 14:31:04 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,13 @@
-#!bin/bash
-# Displays the size of the body of the response of the curl request
-curl -so /dev/null -w '%{size_download}\n' "$1"
+#!/bin/bash
+
+# Get the URL from the command line argument
+url=$1
+
+# Send the request using curl and save the response body to a variable
+response=$(curl -s -w '\n%{size_download}\n' $url)
+
+# Extract the size of the response body from the curl output
+size=$(echo "$response" | tail -n 1)
+
+# Print the size in bytes
+echo "The size of the response body is $size bytes."
