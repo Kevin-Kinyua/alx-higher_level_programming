ceb6e9d68b65a57d626d27dfcb4d0f27e5bbee98
Kevin-Kinyua <w.kevinkinyua@gmail.com>
2023-07-04 04:46:39 -0700

    Task 0: cURL body size Write a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
    The size must be displayed in bytes
    You have to use curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Get the URL from the command line argument
-url=$1
-
-# Send the request using curl and save the response body to a variable
-response=$(curl -s -w '\n%{size_download}\n' $url)
-
-# Extract the size of the response body from the curl output
-size=$(echo "$response" | tail -n 1)
-
-# Print the size in bytes
-echo "The size of the response body is $size bytes."
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
